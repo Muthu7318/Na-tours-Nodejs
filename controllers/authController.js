b3afcf312d316f06eb3bf3b35d4e544d866a4c0e
Muthu7318 <muthumarshal@gmail.com>
2022-01-21 20:41:41 +0530

    setting jwt on cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,7 +13,18 @@ const signToken = (id) =>
 
 const createAndSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
-
+  const cookieOption = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === 'production') {
+    cookieOption.secure = true;
+  }
+  res.cookie('jwt', token, cookieOption);
+  //remove password from output
+  user.password = undefined;
   res.status(statusCode).json({
     status: 'success',
     token: token,
