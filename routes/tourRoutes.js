7741292ad444d2017c3b9cd041a1229d9e4b4992
Muthu7318 <muthumarshal@gmail.com>
2022-01-23 23:02:29 +0530

    merge params in express

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const tourController = require('../controllers/toursController');
 const authController = require('../controllers/authController');
-const reviewController = require('../controllers/reviewController');
+// const reviewController = require('../controllers/reviewController');
+const reviewRouter = require('./reviewRoutes');
 
 const Router = express.Router();
 
 // Router.param('id', tourController.checkId);
+
+// nested routes for reviews
+// Router.route('/:tourId/reviews').post(
+//   authController.protect,
+//   authController.restrictTo('user'),
+//   reviewController.createReview
+// );
+
+Router.use('/:tourId/reviews', reviewRouter);
 
 Router.route('/top-5-cheap').get(
   tourController.aliasTopTours,
@@ -25,10 +35,4 @@ Router.route('/:id')
     tourController.deleteTour
   );
 
-// nested routes for reviews
-Router.route('/:tourId/reviews').post(
-  authController.protect,
-  authController.restrictTo('user'),
-  reviewController.createReview
-);
 module.exports = Router;
