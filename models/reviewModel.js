80ac97117c4e742dcb4aa9be8eb82f0ed937eb02
Muthu7318 <muthumarshal@gmail.com>
2022-01-23 19:52:06 +0530

    get and post review

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 
-const reviewSchema = new mongoose.Schema({
-  review: {
-    type: String,
-    required: [true, 'review cannot be empty'],
-    rating: {
-      type: Number,
-      min: 1,
-      max: 5,
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'review cannot be empty'],
+      rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+      },
     },
     createdAt: {
       type: Date,
