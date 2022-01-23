1dddb183f68c6b162a1e272a74836b59615dd97c
Muthu7318 <muthumarshal@gmail.com>
2022-01-23 18:27:55 +0530

    populte method

--- models/tourmodel.js
+++ models/tourmodel.js
@@ -154,6 +154,15 @@ tourSchema.pre(/^find/, function (next) {
   this.start = Date.now();
   next();
 });
+
+tourSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'guides',
+    select: '-__v -passwordChangedAt',
+  });
+  next();
+});
+
 tourSchema.post(/^find/, function (docs, next) {
   console.log(`query took ${Date.now() - this.start}`);
   // console.log(docs);
