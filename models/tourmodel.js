c2cf62f03432fc027fe08afdb7c81198607b2ee6
Muthu7318 <muthumarshal@gmail.com>
2022-01-25 13:59:02 +0530

    compound index for tour and user

--- models/tourmodel.js
+++ models/tourmodel.js
@@ -46,6 +46,7 @@ const tourSchema = new mongoose.Schema(
       default: 4.5,
       min: [1, 'Rating must above 1.0'],
       max: [5, 'Rating must below 5.0'],
+      set: (value) => Math.round(value * 10) / 10,
     },
     ratingQuantity: {
       type: Number,
