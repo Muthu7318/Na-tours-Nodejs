ae717735bc065826a22afbd9a71acfee277d13d3
Muthu7318 <muthumarshal@gmail.com>
2022-01-28 19:04:37 +0530

    extends in pug

--- app.js
+++ app.js
@@ -74,12 +74,25 @@ app.use((req, res, next) => {
   next();
 });
 // ROUTES
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours',
+  });
+});
+
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker',
+  });
+});
+
 app.get('*', (req, res) => {
   res.status(200).render('base', {
     tour: 'The Forest Hiker',
     user: 'Jonas',
   });
 });
+
 app.use('/api/v1/tours', tourRouter); // this is called as mounting a router
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
